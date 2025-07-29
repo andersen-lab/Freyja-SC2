30ed68f4a615461936ce6f7a0b5e8f97ed144b28
dylanpilz <dpilz@ucsd.edu>
2025-07-29 09:18:52 -0700

    fix metadata parsing to work with ingestion script

--- scripts/aggregate_metadata.py
+++ scripts/aggregate_metadata.py
@@ -1,16 +1,20 @@
 import os
 import pandas as pd
 
-metadata = pd.read_csv('data/all_metadata.tsv', index_col=None, low_memory=False, sep='\t')
+metadata = pd.read_csv('all_metadata.tsv', index_col=None, low_memory=False, sep='\t')
 metadata = metadata[metadata['sample_status'] == 'completed']
 
+# omit ENA samples and limit to USA for now
+metadata = metadata[~metadata['accession'].str.startswith('ERR')]
+metadata = metadata[metadata['geo_loc_country'] == 'USA']
+metadata['Geographic_Location'] = metadata['geo_loc_country'] + '/' + metadata['geo_loc_region']
+metadata = metadata[metadata['Geographic_Location'].notna()]
+
+# drop invalid viral load
 metadata['ww_surv_target_1_conc'] = metadata.apply(
     lambda row: pd.NA if row['ww_surv_target_1_conc_unit'] != 'copies/l' else row['ww_surv_target_1_conc'], axis=1
 )
-
 metadata['ww_surv_target_1_conc'] = metadata['ww_surv_target_1_conc'].apply(lambda x: pd.NA if pd.notna(x) and x <= 0 else x)
-
-metadata['Geographic_Location'] = metadata['geo_loc_country'] + '/' + metadata['geo_loc_region']
 
 # Select relevant columns 
 metadata = metadata[
@@ -25,11 +29,17 @@ metadata = metadata[
     ]
 ]
 
-# add empty columns for future use
-for col in ['Biosample', 'ReleaseDate', 'Isolate_Source', 'Length', 'UpdateDate', 'Isolate_Name', 'Bioprojects', 'Host_OrganismName']:
-    metadata[col] = pd.NA
+# empty string columns
+for col in ['Biosample', 'Isolate_Source', 'Isolate_Name', 'Bioprojects', 'Virus_OrganismName', 'Host_OrganismName']:
+    metadata[col] = 'NA'
 
-metadata['Virus_OrganismName'] = 'SARS-CoV-2'
+# empty date columns
+for col in ['ReleaseDate', 'UpdateDate']:
+    metadata[col] = pd.to_datetime("1970-01-01")
+
+# empty integer columns
+for col in ['Length']:
+    metadata[col] = 0
 
 # Rename columns
 metadata = metadata.rename(
